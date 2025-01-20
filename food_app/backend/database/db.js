1dba562b621d7ae2ff7dc79b969ba8a10123d87d
Minami189 <Soon128HD@gmail.com>
2025-01-20 15:54:53 +0800

    db initalization automation

--- food_app/backend/database/db.js
+++ food_app/backend/database/db.js
@@ -9,5 +9,53 @@ const pool = mysql.createPool({
     password:process.env.PASSWORD,
     database:process.env.DATABASE
 }).promise();
+initializeTables();
+
+async function initializeTables() {
+    try {
+        const [rows] = await pool.query(
+            `SELECT COUNT(*) AS count 
+             FROM information_schema.tables 
+             WHERE table_schema = ? AND table_name = ?`,
+            [process.env.DATABASE, "food"]
+        ); 
+
+        const [rows2] = await pool.query(
+            `SELECT COUNT(*) AS count 
+             FROM information_schema.tables 
+             WHERE table_schema = ? AND table_name = ?`,
+            [process.env.DATABASE, "plan"]
+        ); 
+
+        if (rows[0].count > 0) {
+            console.log("Table food exists");
+        } else {
+            await pool.query(`CREATE TABLE food (
+                            idfood INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            lastEaten DATE DEFAULT NULL,
+                            meat VARCHAR(25) DEFAULT 'UNKNOWN',
+                            name VARCHAR(45) NOT NULL
+                            );`
+                        );
+        }
+
+        if (rows2[0].count > 0) {
+            console.log("Table plan exists");
+        } else {
+            await pool.query(`CREATE TABLE plan (
+                            idplan INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            idfood INT NOT NULL,
+                            FOREIGN KEY (idfood) REFERENCES food(idfood)
+                            );`
+                        );
+        }
+    } catch (err) {
+        console.error("Error checking table existence:", err);
+    }
+}
+
+async function createPlanTable(){
+    
+}
 
 export default pool;
