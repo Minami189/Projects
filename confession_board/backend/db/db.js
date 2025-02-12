7ef1f9ae4af38264bfac3abeeec19915c8bb40a7
Minami189 <Soon128HD@gmail.com>
2025-02-12 19:13:08 +0800

    db file configured

--- confession_board/backend/db/db.js
+++ confession_board/backend/db/db.js
@@ -1,13 +1,8 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
-
-const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-}).promise();
+const urlDB = `mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}`;
+const pool = mysql.createPool(urlDB).promise();
 
 
 export default pool;
